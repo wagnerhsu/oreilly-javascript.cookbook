6cd876c142404eada624b4c6b0a96c987f0eda65
Wagner Xu <wagnerhsu@hotmail.com>
2022-02-04 19:48:21 +0800

    Add files

--- 09-asynchronous/update-page-during-loop/updating-page.js
+++ 09-asynchronous/update-page-during-loop/updating-page.js
@@ -1,48 +1,50 @@
 function doWork() {
-    // Get the <p> element to change
-    const statusElement = document.getElementById('status');
-  
-    // Track the time and the number of passes through the loop
-    const startTime = Date.now();
-    let counter = 0;
-  
-    statusElement.innerText = 'Processing started';
+  // Get the <p> element to change
+  const statusElement = document.getElementById('status');
 
-    while ((Date.now() - startTime < 10000)) {
-      counter += 1;
-      statusElement.innerText = `Just generated number ${counter}`;
-    }
+  // Track the time and the number of passes through the loop
+  const startTime = Date.now();
+  let counter = 0;
 
-    statusElement.innerText = 'Processing completed';
+  statusElement.innerText = 'Processing started';
+
+  while ((Date.now() - startTime < 10000)) {
+    counter += 1;
+    statusElement.innerText = `Just generated number ${counter}`;
   }
 
-  function doWorkInChunks() {
-    // Get the <p> element to change
-    const statusElement = document.getElementById('status');
- 
-    // Track the time and the number of passes through the loop
-    const startTime = Date.now();
-    let counter = 0;
- 
-    statusElement.innerText = 'Processing started';
- 
-    // Create an anonymous function that does one chunk of work
-    const doChunkedTask = () => {
-       if (Date.now() - startTime < 10000) {
-         counter += 1;
-         statusElement.innerText = `Just generated number ${counter}`;
- 
-         // Call the function again, for the next chunk
-         window.setTimeout(doChunkedTask, 0);
-       }
-       else {
-         statusElement.innerText = 'Processing completed';
-      }
-    };
- 
-   // Start the process by calling the function for the first time
-   doChunkedTask();
- }
+  statusElement.innerText = 'Processing completed';
+}
+
+function doWorkInChunks() {
+  // Get the <p> element to change
+  const statusElement = document.getElementById('status');
+
+  // Track the time and the number of passes through the loop
+  const startTime = Date.now();
+  let counter = 0;
+
+  statusElement.innerText = 'Processing started';
+
+  // Create an anonymous function that does one chunk of work
+  const doChunkedTask = () => {
+    if (Date.now() - startTime < 10000) {
+      counter += 1;
+      const progressText = `Just generated number ${counter}`
+      statusElement.innerText = progressText;
+      console.log(progressText)
+      // Call the function again, for the next chunk
+      window.setTimeout(doChunkedTask, 0);
+    }
+    else {
+      statusElement.innerText = 'Processing completed';
+      console.log("done");
+    }
+  };
+
+  // Start the process by calling the function for the first time
+  doChunkedTask();
+}
 
 document.getElementById('doWork').addEventListener('click', doWork);
 document.getElementById('doWorkInChunks').addEventListener('click', doWorkInChunks);
