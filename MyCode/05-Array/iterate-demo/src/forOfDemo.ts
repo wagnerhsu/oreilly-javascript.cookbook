b06cf350205e754d08a50f7b40520ce3c6a00ee0
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-28 15:36:50 +0800

    update file

--- MyCode/05-Array/iterate-demo/src/forOfDemo.ts
+++ MyCode/05-Array/iterate-demo/src/forOfDemo.ts
@@ -2,7 +2,7 @@ import assert from 'assertion'
 
 let iterable = [3, 5, 7];
 
-const arrResult = new Array<string>();
+const arrResult = new Array<number>();
 
 for (const i of iterable) {
   arrResult.push(i);
