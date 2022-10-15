4cd4faba2ce52a65563eb4e27229a7c961bd458d
Wagner Xu <wagnerhsu@hotmail.com>
2022-10-15 23:25:55 +0800

    update files

---
+++ MyCode/05-Array/map-collection/main.ts
@@ -0,0 +1,30 @@
+const products = new Map();
+
+// Add three items
+products.set('RU007', { name: 'Rain Racer 2000', price: 1499.99 });
+products.set('STKY1', { name: 'Edible Tape', price: 3.99 });
+products.set('P38', { name: 'Escape Vehicle (Air)', price: 2999.0 });
+
+// Check for two items using the item code
+console.log(products.has('RU007')); // true
+console.log(products.has('RU494')); // false
+
+// Retrieve an item
+const product = products.get('P38');
+if (typeof product !== 'undefined') {
+  console.log(product.price); // 2999
+}
+
+// Remove the Edible Tape item
+products.delete('STKY1');
+console.log(products.size); // 2
+
+const array = Array.from(products);
+for (const product in array) {
+  console.log(product, array[product]);
+}
+
+products.forEach((x) => console.log(x));
+for (const product of products) {
+  console.log(product);
+}
