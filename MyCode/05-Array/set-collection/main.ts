9d67358fcb9924443dc0b3e6b7e15c3bca48b755
Wagner Xu <wagnerhsu@hotmail.com>
2022-10-15 23:03:22 +0800

    update prettier config

--- MyCode/05-Array/set-collection/main.ts
+++ MyCode/05-Array/set-collection/main.ts
@@ -1,5 +1,7 @@
 const animals = new Set(['elephant', 'tiger', 'lion', 'zebra', 'cat', 'dog']);
 
+console.log('List Set using for...of');
 for (const animal of animals) {
   console.log(animal);
 }
+Array.from(animals).forEach((x) => console.log(x));
