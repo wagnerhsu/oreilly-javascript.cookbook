c799e1d22044445cec82a58d240450d60c730c0a
Wagner Xu <wagnerhsu@hotmail.com>
2022-11-18 23:16:23 +0800

    update files

--- MyCode/05-Array/iterate-demo/src/forOfDemo.ts
+++ MyCode/05-Array/iterate-demo/src/forOfDemo.ts
@@ -8,3 +8,15 @@ for (const i of iterable) {
   arrResult.push(i);
 }
 assert.deepEqual(arrResult, [3, 5, 7]);
+
+let flag = iterable.some(x=>x>4);
+assert.equal(flag,true);
+flag = false;
+for(const i of iterable) {
+  if (i > 4) {
+    console.log(i);
+    flag = true;
+    break;
+  }
+}
+assert.equal(flag,true);
